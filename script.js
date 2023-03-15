542292fd2cd0af13572220bf037d46dbde80dab3
G-DevendraBabu <116060950+G-DevendraBabu@users.noreply.github.com>
2023-03-15 17:04:17 +0530

    commit1

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 //your JS code here. If required.
+
+//selecting the input and button elemnets
+
+let name1=document.getElementById("name");
+let age=document.getElementById("age");
+let butn=document.getElementById("btn");
+
+butn.addEventListener("click" ,()=>{
+
+    if(name1.value !="" || age.value !="")
+    {      
+            let p1= new Promise((resolve,reject)=>{
+                setTimeout(()=>{
+                    if(age.value>=18)
+                    {
+                        resolve(`Welcome, ${name1.value}. you can vote`);
+                    }
+                    else
+                    {
+                        reject(`oh sorry ${name1.value}. You aren't old enough `);
+                    }
+                },4000)
+            })
+            p1.then((data)=>{console.log(data);})
+
+    }
+
+})
